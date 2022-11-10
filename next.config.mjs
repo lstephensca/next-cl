1fb10b7e1243e359984f81f5d71c6ded9a19fa48
Louis Stephens <lstephens@chumneyads.com>
2022-11-10 08:58:30 -0500

    Freshly squeezed

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+// Contentlayer config
+import { withContentlayer } from "next-contentlayer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	experimental: {
+		appDir: true,
+	},
+};
+
+export default withContentlayer(nextConfig);
